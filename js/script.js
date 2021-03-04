076bcd14559419c49e669b7a1e46ea3a767fc813
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-03-04 22:10:53 +0100

    Added script to computer movements

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 
 let playerMove = `papier`;
 printMessage ('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+// Ćwiczenie dodatkowe: losowanie liczby od 11 do 19
+
+let randomNumber = Math.floor(Math.random() * 9 + 11);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+
+// Zadanie: Odczytywanie ruchów komputera
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+} else if (randomNumber == 2){
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
